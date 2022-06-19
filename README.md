7fc07727684e488ffd3596f9e38759c95a16d324
Nina Kalinina <nkali@pm.me>
2022-06-19 21:44:06 +0100

    md2txt tool for old computers

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 This project aims to create an open framework for personal computing. Cheap, simple, yet practical.
 
-![alt text](https://github.com/ninakali/flourish-pc/blob/main/blobs/demo.jpg "Demo of the reference implementation")
+![(There used to be a picture of a computer.)](https://github.com/ninakali/flourish-pc/blob/main/blobs/demo.jpg "Demo of the reference implementation")
 
-# why?
+## why?
 
 Two main reasons.
 
@@ -12,13 +12,13 @@ First, it seems [the winter](http://collapseos.org/why.html) is coming. Some pro
 
 Second, in many places on this planet __the spring__ has never come in the first place. Even in countries where computer-like devices are common, they usually cannot be used for general-purpose computing easily, nor can they be hacked/repurposed without extensive know-how. One might argue that the adventure of under-$1 powerful ARM chips and easy-to-hack devices like Arduino has made personal computing affordable and straightforward. But unfortunately, one still needs a PC to flash the Arduino.
 
-# so, what are you going to do about it?
+## so, what are you going to do about it?
 
 Suppose Collapse OS is a framework for making bootable OS for scavenge-friendly platforms. In that case, Flourish PC is a framework for building a useful personal computing device using elements one has at hand. Therefore, it's less about specific solutions (though we are planning to provide you with multiple reference implementations) and more about sharing the know-how required for building a computer.
 
 It is challenging to decide what the framework should include, though. For example, should we cover topics ranging from "you have a stick and a stone" to "now we finally produced electron tubes/semiconductor devices"? Probably not; we believe there must be better sources of knowledge regarding those essential-for-preserving-civilization topics. But on the other hand, basic knowledge about digital electronics is necessary for building a computer so we will include a primer on this topic.
 
-# structure of the project
+## structure of the project
 
 The __src__ directory is for the framework: documentation in Markdown format, schematics and code in source and target formats (for instance, kicad+png).
 
@@ -26,9 +26,9 @@ The __tools__ directory is for tools needed to convert the sources of the framew
 
 The __blobs__ directory is for files used in this file.
 
-# community?
+## community?
 At the moment, there is no community around this project. If you are interested in Flourish PC and want to get in touch with the creators, feel free to email Nina at nkali[at]pm[dot]me or contact her on [Twitter](https://twitter.com/nina_kali_nina).
 
 
-# ps
+## ps
 Anime girl art by [Sutemo](https://sutemo.itch.io/female-character)
---
+++ tools/md2txt.py
@@ -0,0 +1,89 @@
+import sys
+from mistletoe import markdown, base_renderer, block_token
+
+class TxtRenderer(base_renderer.BaseRenderer):
+    MAXLEN = 63
+    listblocks = 0
+
+    def render_heading(self, token):
+        if token.level == 1:
+            template = '=== {} '
+            ready = template.format(self.render_inner(token))
+            ready = '\n' + ready + '=' * (self.MAXLEN - len(ready)) + "\n"
+            ready = '\n' + '=' * self.MAXLEN + ready + '=' * self.MAXLEN + '\n'
+        elif token.level == 2:
+            template = '*** {} '
+            ready = template.format(self.render_inner(token))
+            ready = '\n' + ready + "*" * (self.MAXLEN - len(ready)) + "\n"
+        else:
+            template = '\n! {}\n'
+            ready = template.format(self.render_inner(token))
+            ready = ready + "~" * (len(ready)-2) + "\n"
+        return ready
+
+    def render_strong(self, token):
+        template = '*{}*'
+        return template.format(self.render_inner(token))
+
+    def render_emphasis(self, token):
+        template = '_{}_'
+        return template.format(self.render_inner(token))
+
+    def render_list_item(self, token):
+        template = ' > {}'
+        return template.format(self.render_inner(token))
+
+    def render_link(self, token):
+        template = '[ {inner} ] =>{target}<='
+        if token.target.endswith('.md'):
+            target = token.target[:-3] + ".txt"
+        else:
+            target = token.target  # YOLO
+        inner = self.render_inner(token)
+        return template.format(target=target, inner=inner)
+
+    def cute_split(self, line):
+        cute = []
+        tmp = []
+        for ch in line:
+            if len(tmp) < self.MAXLEN:
+                tmp.append(ch)
+            else:
+                idx = len(tmp) - tmp[::-1].index(" ") - 1
+                if idx > 1:
+                    cute.append(''.join(tmp[:idx]))
+                    tmp = tmp[idx+1:]
+                    tmp.append(ch)
+                else:
+                    # shikatanai yo
+                    cute.append(''.join(tmp))
+                    tmp = [ch]
+        cute.append(''.join(tmp))
+        return cute
+
+
+    def render_inner(self, token):
+        if isinstance(token, block_token.List):
+            self.listblocks += 1
+        rendered = [self.render(child) for child in token.children]
+        if isinstance(token, block_token.List):
+            self.listblocks -= 1
+        return ''.join(rendered)
+
+    def render_paragraph(self, token):
+        inner = ("  " + self.render_inner(token)).splitlines()
+        new_inner = []
+        for line in inner:
+            if len(line) > self.MAXLEN:
+                for subline in self.cute_split(line):
+                    new_inner.append(subline)
+            else:
+                new_inner.append(line)
+        return '{}'.format('\n'.join(new_inner)) + self._block_eol()
+
+    def _block_eol(self):
+        return '\n' if self.listblocks > 0 else '\n\n'
+
+with open(sys.argv[1], 'r', encoding='utf-8') if len(sys.argv) == 2 else sys.stdin as infile:
+     rendered = markdown(infile, TxtRenderer)
+     print(rendered)
